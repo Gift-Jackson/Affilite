50112afb5fce5952f382f421df8f5073ed850a00
Gift-Jackson <113178450+Gift-Jackson@users.noreply.github.com>
2023-06-04 07:33:32 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,36 @@
+const header = document.querySelector("header");
+const toggle = document.querySelector(".toggle-box");
+const toggleBar = document.querySelector(".toggle");
+const ul = document.querySelector("ul");
+const link = document.querySelectorAll(".link");
+
+let lastScroll = 0;
+
+window.addEventListener("scroll", ()=>{
+    if(scrollY > lastScroll){
+        header.style.top = "-100px";
+    }
+    else{
+        header.style.top = "0";
+    }
+
+    lastScroll = window.scrollY;
+});
+
+toggle.addEventListener("click", ()=>{
+    toggleBar.classList.toggle("active");
+
+    if(toggleBar.classList.contains("active")){
+        ul.classList.add("active");
+    }
+    else{
+        ul.classList.remove("active");
+    }
+});
+
+link.forEach((e)=>{
+    e.addEventListener("click", ()=>{
+        ul.classList.remove("active");
+        toggleBar.classList.remove("active");
+    })
+});
